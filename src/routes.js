fb25befbf556ebc6da2fa561e3aec225f1fc25fa
AlbertoAuttic <albertoauttic@gmail.com>
2022-04-19 13:39:08 +0200

    De momento va todo bien

--- src/routes.js
+++ src/routes.js
@@ -2,13 +2,9 @@ import { Navigate, useRoutes } from 'react-router-dom';
 // layouts
 import DashboardLayout from './layouts/dashboard';
 import LogoOnlyLayout from './layouts/LogoOnlyLayout';
-//
-import Blog from './pages/Blog';
-import User from './pages/User';
 import Login from './pages/Login';
 import NotFound from './pages/Page404';
 import Register from './pages/Register';
-import Products from './pages/Products';
 import DashboardApp from './pages/DashboardApp';
 
 // ----------------------------------------------------------------------
@@ -18,12 +14,7 @@ export default function Router() {
     {
       path: '/dashboard',
       element: <DashboardLayout />,
-      children: [
-        { path: 'app', element: <DashboardApp /> },
-        { path: 'user', element: <User /> },
-        { path: 'products', element: <Products /> },
-        { path: 'blog', element: <Blog /> },
-      ],
+      children: [{ path: 'app', element: <DashboardApp /> }],
     },
     {
       path: '/',
